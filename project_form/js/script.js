b8949c5bb8ce63502ccc92e5590baba70cd30e3d
Kelly Cruz <kellycruz.ti@gmail.com>
2023-07-12 14:22:41 -0400

    Create form

--- project_form/js/script.js
+++ project_form/js/script.js
@@ -1,16 +1,25 @@
 function meuEscopo(){
   const form = document.querySelector('.form');
+  const resultado = document.querySelector('.resultado');
 
-  // form.onsubmit = function (evento) {
-  //   evento.preventDefault();
-  //   alert(1);
-  //   console.log('Foi enviado!');
-  // };
-  let contador = 1;
+  const pessoas = [];
+
   function recebeEventoForm(evento) {
     evento.preventDefault();
-    console.log(`Form não foi enviado!${contador}`);
-    contador++;
+    const nome = form.querySelector('.nome');
+    const sobrenome = form.querySelector('.sobrenome');
+    const peso = form.querySelector('.peso');
+    const altura = form.querySelector('.altura');
+
+    pessoas.push({
+      nome: nome.value,
+      sobrenome: sobrenome.value,
+      peso: peso.value,
+      altura: altura.value
+    });
+
+    resultado.innerHTML += `<p>${nome.value} ${sobrenome.value} ${peso.value} ${altura.value}</p>`
+    
   }
 
   form.addEventListener('submit', recebeEventoForm);
